4cb238a9a5af7e243a01918228d5bf542ede869e
hongg <hongg@DESKTOP-6S2BMHA>
2016-07-14 00:21:32 +0800

    添加字符识别

--- Src/IKAnalyzer.Core/Config/Configuration.cs
+++ Src/IKAnalyzer.Core/Config/Configuration.cs
@@ -1,4 +1,4 @@
-ï»¿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace IKAnalyzer.Config
 {
     /// <summary>
-    /// é…ç½®ç®¡ç†ç±»æ¥å£
+    /// ÅäÖÃ¹ÜÀíÀà½Ó¿Ú
     /// </summary>
     /// <remarks>
     /// 
@@ -15,24 +15,24 @@ namespace IKAnalyzer.Config
     public interface Configuration
     {
         /// <summary>
-        /// UseSmartæ ‡å¿—ä½
-        /// UseSmart=trueï¼Œåˆ†è¯å™¨ä½¿ç”¨æ™ºèƒ½åˆ‡åˆ†ç­–ç•¥,=falseåˆ™ä½¿ç”¨ç»†ç²’åº¦åˆ‡åˆ†
+        /// UseSmart±êÖ¾Î»
+        /// UseSmart=true£¬·Ö´ÊÆ÷Ê¹ÓÃÖÇÄÜÇĞ·Ö²ßÂÔ,=falseÔòÊ¹ÓÃÏ¸Á£¶ÈÇĞ·Ö
         /// </summary>
         bool UseSmart { get; set; }
         /// <summary>
-        /// è·å–ä¸»è¯å…¸è·¯å¾„
+        /// »ñÈ¡Ö÷´ÊµäÂ·¾¶
         /// </summary>
         string MainDictionary { get; }
         /// <summary>
-        /// è·å–é‡è¯è¯å…¸è·¯å¾„
+        /// »ñÈ¡Á¿´Ê´ÊµäÂ·¾¶
         /// </summary>
         string QuantifierDictionary { get; }
         /// <summary>
-        /// è·å–æ‰©å±•å­—å…¸é…ç½®è·¯å¾„
+        /// »ñÈ¡À©Õ¹×ÖµäÅäÖÃÂ·¾¶
         /// </summary>
-        List<string> ExtDictionarys { get;  }
+        List<string> ExtDictionarys { get; }
         /// <summary>
-        /// è·å–æ‰©å±•åœæ­¢è¯å…¸é…ç½®è·¯å¾„
+        /// »ñÈ¡À©Õ¹Í£Ö¹´ÊµäÅäÖÃÂ·¾¶
         /// </summary>
         List<string> ExtStopWordDictionarys { get; }
     }
